f954e0fd5e0e0ddbc3d6301934011972d2c4edd7
erje <eedwa_01@uni-muenster.de>
2012-02-19 01:45:58 +0100

    vectorized versions of omega and dwdk. boris_np rewritten with vectorized functions

--- boris_lib.py
+++ boris_lib.py
@@ -1,4 +1,113 @@
 import math
+from numpy import sqrt, sin, cos, exp, pi
+
+# omega built for numpy vectorization
+def vomega(kzeta, phi, mp):
+	Ms, H, L, gamma, alpha, theta, n = mp
+
+	wh = gamma*H
+	wm = gamma*Ms
+
+	# Exception for FMR
+	#if kzeta == 0.0:
+		#return math.sqrt(wh*(wh + wm))
+	
+	kappa_n = n*pi/L
+	kn = sqrt(kzeta**2 + kappa_n**2)
+	Q = (wh + alpha*wm*kn*kn)
+	
+	# Fn, same for totally unpinned and totally pinned cases
+	Fn = (2/(kzeta*L))*(1 - ((-1)**n)*exp(-kzeta*L))
+
+	# Pnn, totally UNPINNED surface spins
+	if n == 0:
+		B = 0.5
+	else:
+		B = 1
+	
+	Pnn = kzeta**2/(kn**2) - Fn*B*kzeta**4/(kn**4)
+
+	A = sin(theta)*sin(theta)
+	C = cos(phi)*cos(phi)
+	D = sin(phi)*sin(phi)
+	Fnn_1 = Pnn
+	Fnn_2 = A
+	Fnn_3 = -A*Pnn*(1 + C)
+	Fnn_4 = A*wm*Pnn*(1 - Pnn)*D/Q
+	Fnn = (Fnn_1 + Fnn_2 + Fnn_3 + Fnn_4)
+
+	return (sqrt(Q*(Q + wm*Fnn)))
+
+# group velocity, numpy vectorization
+def vdwdk(kzeta, phi, mp):
+	Ms, H, L, gamma, alpha, theta, n = mp
+
+	wh = gamma*H
+	wm = gamma*Ms
+
+	kn = sqrt(kzeta**2 + (n*pi/L)**2)
+	dkndk = (kzeta/kn)
+
+	Q = (wh + alpha*wm*kn*kn)
+	dQdk = (2*alpha*wm*kzeta)
+
+	Fn = ((2/(kzeta*L))*(1 - ((-1)**n)*math.exp(-kzeta*L)))
+
+	dFndk_1 = -2/(L*kzeta*kzeta)
+	dFndk_2 = 2*((-1)**n)*exp(-kzeta*L)/(kzeta*kzeta*L)
+	dFndk_3 = 2*((-1)**n)*exp(-kzeta*L)/kzeta
+	dFndk = (dFndk_1 + dFndk_2 + dFndk_3)
+
+	if n==0:
+		B = 0.5
+	else:
+		B = 1.0
+
+	Pnn = (kzeta**2/(kn**2) - Fn*B*kzeta**4/(kn**4))
+
+	A = sin(theta)*sin(theta)
+	C = cos(phi)*cos(phi)
+	D = sin(phi)*sin(phi)
+	Fnn_1 = Pnn
+	Fnn_2 = A
+	Fnn_3 = -A*Pnn*(1 + C)
+	Fnn_4 = A*wm*Pnn*(1 - Pnn)*D/Q
+	Fnn = (Fnn_1 + Fnn_2 + Fnn_3 + Fnn_4)
+
+	dPnndk_1 = 2*kzeta*(kn**-2)
+	dPnndk_2 = -2*(kzeta**2)*(kn**-3)*dkndk
+	dPnndk_3 = -4*(kzeta**3)*(kn**-4)*B*Fn
+	dPnndk_4 = 4*(kzeta**4)*(kn**-5)*B*Fn*dkndk
+	dPnndk_5 = -(kzeta**4)*(kn**-4)*B*dFndk
+	dPnndk = (dPnndk_1 + dPnndk_2 + dPnndk_3 + dPnndk_4 + dPnndk_5)
+
+	E = wm*sin(theta)*sin(theta)*sin(phi)*sin(phi)
+	dFnndk_1 = dPnndk
+	dFnndk_2 = -dPnndk*sin(theta)*sin(theta)*(1 + cos(phi)*cos(phi))
+	dFnndk_3 = - (1/Q)*(1/Q)*E*Pnn*dQdk
+	dFnndk_4 = (1/Q)*E*dPnndk
+	dFnndk_5 = (1/Q)*(1/Q)*E*Pnn*Pnn*dQdk
+	dFnndk_6 = -2*(1/Q)*E*Pnn*dPnndk
+	dFnndk = (dFnndk_1 + dFnndk_2 + dFnndk_3 + dFnndk_4 + dFnndk_5 + dFnndk_6)
+
+	# We use our own omega here, since it's simple and calling omega_n costs time
+	omega = sqrt(Q*(Q + wm*Fnn))
+
+	dbetadk = dQdk*(2*Q + wm*Fnn) + wm*Q*dFnndk
+	M = wm*sin(theta)*sin(theta)*Pnn*(1 - Pnn)/Q
+	
+	dwdkzeta = (1/(2*omega))*dbetadk
+	# note change due to henning's check, compare with previous results
+	dwdphi = (1/(2*omega))*Q*wm*sin(2*phi)*(Pnn*sin(theta)*sin(theta) + M)
+
+	dwdkz = dwdkzeta*(cos(phi)) - (1/kzeta)*dwdphi*sin(phi)
+	dwdky = dwdkzeta*(sin(phi)) + (1/kzeta)*dwdphi*cos(phi)
+
+	# Have to multiply by 2pi, since we work with the gyromagnetic ratio
+	# in Hz/Oe...i.e. we work with normal gamma divided by 2pi
+	# This 2pi yields real group velocity
+	return [2*pi*dwdkz, 2*pi*dwdky, 2*pi*dwdkzeta, 2*pi*dwdphi]
+
 # frequency
 def omega(kzeta, phi, Ms, H, L, gamma, alpha, theta, n):
 	pi = math.pi
--- boris_np.py
+++ boris_np.py
@@ -2,141 +2,112 @@
 
 from numpy import *
 import matplotlib.pyplot as plt
-from boris_lib import omega
-from boris_lib import dwdk
-from boris_lib import d2wdk2
-import math
+from boris_lib import omega, vomega, dwdk, d2wdk2
 
-pi = math.pi
-
+# material parameters
 Ms = 139260.5752 
 H = 55704.230082
 L = 5.1e-06
 gamma = 35185.83772
 alpha = 3.1e-16
-phi_selected = 0.0
-theta = 90.0
-W = 0.0013
-k_max = 1000.0
-freq = 4.2
+theta = deg2rad(90.0)
 trans_n = 0.0
+mp = [Ms, H, L, gamma, alpha, theta, trans_n]
 
-# Unit conversions from set_the_boris to what the routines need
-# degrees -> radians
-phi_selected = phi_selected * pi/180
-phi = phi_selected
-# degrees -> radians
-theta = theta * pi/180
+# k_max = no of nodes*smallest k for sample width W
+W = 0.0013
+k0 = pi/W
+k_max = 100.0*k0
+
+# freq, epsilon for emission pattern [GHz]
+freq = 3.65
+epsilon = 0.01
 # GHz -> Hz
 freq = freq*10**9
+epsilon = epsilon*10**9
 
-print('Calculating 1D dispersion...')
-k0 = 2*pi/W
-k = []
-w1 = []
-w2 = []
-ssf = 100
-for i in range(0,int(k_max + 1), int(k_max/ssf)):
-	kzeta = k0*i
-	# Frequency written to file in GHz, wavevector in inverse centimeters
-	# Step size determined by film thickness, equal to 2*pi/W
-	#disp_out.write(str(kzeta*10**-2) + '\t' + str(omg_temp*10**-9) + '\n')
-	k.append(10**-2*kzeta)
-	phi = 0.0
-	w1.append(10**-9*omega(kzeta, phi, Ms, H, L, gamma, alpha, theta, trans_n))
-	phi = 90*pi/180
-	w2.append(10**-9*omega(kzeta, phi, Ms, H, L, gamma, alpha, theta, trans_n))
-
+#print('Calculating 1D dispersion...')
 plt.subplot(221)
 plt.title('1D Dispersion')
 plt.xlabel('Wavevector (1/cm)')
 plt.ylabel('Frequency (GHz)')
 plt.grid(True)
-plt.plot(k,w1,'b-',k,w2,'r-')
-plt.tight_layout()
-#plt.show()
-print('Done calculating 1D dispersion!')
 
-print('Calculating group velocity vector field...')
-kzl = []
-kyl = []
-dwdkz = []
-dwdky = []
-ssf = 20
-for i in range(0, int(k_max + 1), int(k_max/ssf)):
-	kz = k0*(2*i-k_max)
-	for l in range(0, int(k_max + 1), int(k_max/ssf)):
-		ky = k0*(2*l-k_max)
-		if kz == 0 and ky == 0:
-			continue
+nsteps = 100
+k_BV = linspace(0,k_max,nsteps)
+k_DE = linspace(0,k_max,nsteps)
+# we plot k in 1/cm and omega in GHz
+plt.plot(10**-2*k_BV,10**-9*vomega(k_BV,0.0,mp),'b-',10**-2*k_DE,10**-9*vomega(k_DE,pi/2,mp),'r-')
 
-		kzeta = math.sqrt(kz**2 + ky**2)
-		phi = math.atan2(kz,ky)
-		dwdk_temp = dwdk(kzeta, phi, Ms, H, L, gamma, alpha, W, theta, k_max, trans_n)
+#print('Done calculating 1D dispersion!')
 
-		kzl.append(kz)
-		kyl.append(ky)
-		dwdkz.append(dwdk_temp[0])
-		dwdky.append(dwdk_temp[1])
+#print('Calculating group velocity vector field...')
+#plt.subplot(222)
+#plt.title('Group Veloctiy')
+#plt.xlabel('kz (1/m)')
+#plt.ylabel('ky (1/m)')
+#plt.grid(True)
 
+#kzl = []
+#kyl = []
+#dwdkz = []
+#dwdky = []
+#ssf = 20
+#for i in range(0, int(k_max + 1), int(k_max/ssf)):
+	#kz = k0*(2*i-k_max)
+	#for l in range(0, int(k_max + 1), int(k_max/ssf)):
+		#ky = k0*(2*l-k_max)
+		#if kz == 0 and ky == 0:
+			#continue
+#
+		#kzeta = math.sqrt(kz**2 + ky**2)
+		#phi = math.atan2(kz,ky)
+		#dwdk_temp = dwdk(kzeta, phi, Ms, H, L, gamma, alpha, W, theta, k_max, trans_n)
+#
+		#kzl.append(kz)
+		#kyl.append(ky)
+		#dwdkz.append(dwdk_temp[0])
+		#dwdky.append(dwdk_temp[1])
+#
+#plt.quiver(kzl, kyl, dwdkz, dwdky)
+#print('Done calculating group velocity vector field!')
+
+#print('Calculating dispersion surface')
 plt.subplot(222)
-plt.title('Group Veloctiy')
-plt.xlabel('kz (1/m)')
-plt.ylabel('ky (1/m)')
-plt.grid(True)
-plt.quiver(kzl, kyl, dwdkz, dwdky)
-print('Done calculating group velocity vector field!')
+plt.title('Dispersion Surface')
+plt.xlabel('kpara (1/m)')
+plt.ylabel('kperp (1/m)')
 
-print('Calculating dispersion surface')
-# We want to write a function that calcualtes the
-# dispersion surface from -k_max to k_max in both
-# direction, with a given step_size
-# Step-size factor
-ssf = 100
-# What follows works, but abomination much?
-c = zeros(( len(range(0, int(k_max + 1), int(k_max/ssf))), len(range(0, int(k_max + 1), int(k_max/ssf))) ))
-d = zeros(( len(range(0, int(k_max + 1), int(k_max/ssf))), len(range(0, int(k_max + 1), int(k_max/ssf))) ))
-#c[::,100] = range(0, int(k_max + 1), 10)
-
-kperp = zeros(( len(range(0, int(k_max + 1), int(k_max/ssf))), len(range(0, int(k_max + 1), int(k_max/ssf))) ))
-kpara = zeros(( len(range(0, int(k_max + 1), int(k_max/ssf))), len(range(0, int(k_max + 1), int(k_max/ssf))) ))
-testest = 0
-for i in range(len(kpara)):
-	kpara[i] = range(int(-k_max/2), int(k_max/2 + 1), int(k_max/ssf))
-for i in range(len(kperp)):
-	kperp[i] = kperp[i] + k_max - testest - k_max/2
-	testest += k_max/ssf
+gridpts = 100
+kpara, kperp = meshgrid(linspace(-k_max,k_max,gridpts), linspace(-k_max,k_max,gridpts))
 disp_surface_limits = [ kpara.min(), kpara.max(), kperp.min(), kperp.max() ]
 kzeta = sqrt(kperp**2 + kpara**2)
 phi = arctan2(kperp,kpara)
-for i in range(len(kzeta)):
-	for j in range(len(kzeta[i])):
-		c[i,j] = omega(kzeta[i,j],phi[i,j],Ms,H,L,gamma,alpha,theta,trans_n)
-		#d[i,j] = linalg.det(d2wdk2(kzeta[i,j],phi[i,j],Ms,H,L,gamma,alpha, W, theta, k_max, trans_n))
-plt.subplot(223)
-#plt.imshow(c[::-1,:])
-#plt.contour(c[::-1,:])
-plt.title('Dispersion Surface')
-plt.xlabel('kz (1/m)')
-plt.ylabel('ky (1/m)')
-plt.imshow(c[::-1,:], extent = disp_surface_limits )
-plt.contour(c[::-1,:], extent = disp_surface_limits)
+vo = vomega(kzeta,phi,mp)
+plt.imshow(vo, extent = disp_surface_limits)
+plt.contour(vo, extent = disp_surface_limits)
 
-print('Done calculating dispersion surface!')
+#print('Minimum frequency: ' + str(10**-9*vo.min()) + ' GHz')
+#print('Maximum frequency: ' + str(10**-9*vo.max()) + ' GHz')
+#print('Difference: ' + str(10**-9*(vo.max() - vo.min())) + ' GHz')
+#print('Done calculating dispersion surface!')
+
+#print('Calculating slowness surface...')
+plt.subplot(223)
+plt.title('Slowness Surface, ' + str(round(10**-9*freq,2)) + ' GHz')
+plt.xlabel('kpara (1/m)')
+plt.ylabel('kperp (1/m)')
+
+lowb = freq - epsilon
+highb = freq + epsilon
+iso = where(vo < lowb, 0, where(vo > highb, 0, vo))
+plt.imshow(iso, extent = disp_surface_limits)
+#print('Done calculating slowness surface!')
 
 plt.subplot(224)
-print('Calculating slowness surface...')
-flip = c[::-1,:].copy()
-freq = 4.0*10**9
-epsilon = 0.01*10**9
-for i in range(len(flip)):
-	for j in range(len(flip[i])):
-		if flip[i,j] < (freq - epsilon) or flip[i,j] > (freq + epsilon):
-			#print(flip[i,j])
-			flip[i,j] = 0
+plt.title('Emission Pattern')
+plt.imshow(abs(fft.ifftshift(fft.ifft2(iso)))**2)
 
-plt.imshow(abs(fft.ifftshift(fft.ifft2(flip)))**2)
-print('Done calculating slowness surface!')
-
+print(disp_surface_limits)
 plt.tight_layout()
 plt.show()
