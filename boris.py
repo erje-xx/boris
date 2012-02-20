8cf3d25312083ce4edc70d4612095346e92a4266
erje <eedwa_01@uni-muenster.de>
2012-02-20 22:11:04 +0100

    added loop for multiple emission patterns

--- boris.py
+++ boris.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import matplotlib.pyplot as plt
-from numpy import pi, deg2rad, linspace, meshgrid, sqrt, arctan2, where, fft 
+from numpy import pi, deg2rad, linspace, meshgrid, sqrt, arctan2, where, fft, angle
 from boris_lib import vomega, vdwdk
 
 # material parameters
@@ -17,20 +17,25 @@ mp = [Ms, H, L, gamma, alpha, theta, trans_n]
 # k_max = no of nodes*minimum k for sample width W
 W = 0.0013
 k0 = pi/W
-k_max = 100.0*k0
+nodes = 100.0
+k_max = nodes*k0
 
 # freq, epsilon for emission pattern [GHz]
 freq = 3.65
-epsilon = 0.01
+epsilon = 0.005
 # GHz -> Hz
 freq = freq*10**9
 epsilon = epsilon*10**9
 
 # Number of points for dispersion surface & group velocity
-gridpts_ds = 100
+# Corresponds to sample rate in k-space, gridpts_ds
+gridpts_ds = 2000
 gridpts_vg = 10
 
-plt.subplot(221)
+#summary figure number
+sfn= 1
+plt.figure(sfn)
+plt.subplot(121)
 plt.title('Dispersion Surface')
 plt.xlabel('kpara [1/m]')
 plt.ylabel('kperp [1/m]')
@@ -43,7 +48,7 @@ disp_surface_limits = [ kpara.min(), kpara.max(), kperp.min(), kperp.max() ]
 plt.imshow(vo, extent = disp_surface_limits)
 plt.contour(vo, extent = disp_surface_limits)
 
-plt.subplot(222)
+plt.subplot(122)
 plt.title('Group Veloctiy')
 plt.xlabel('kz [1/m]')
 plt.ylabel('ky [1/m]')
@@ -56,23 +61,57 @@ vd_z = vdwdk(kzeta,phi,mp)[0]
 vd_y = vdwdk(kzeta,phi,mp)[1]
 plt.quiver(kpara, kperp, vd_z, vd_y)
 
-plt.subplot(223)
-plt.title('Slowness Surface, ' + str(round(10**-9*freq,2)) + ' GHz')
-plt.xlabel('kpara [1/m]')
-plt.ylabel('kperp [1/m]')
+# number of emission patterns
+no_of_ep = 10
+ep_freqs= linspace(vo.min(),vo.max(),no_of_ep)
 
-lowb = freq - epsilon
-highb = freq + epsilon
-iso = where(vo < lowb, 0, where(vo > highb, 0, vo))
-plt.imshow(iso, extent = disp_surface_limits)
-
-plt.subplot(224)
-plt.title('Emission Pattern')
-plt.xlabel('para [m]')
-plt.ylabel('perp [m]')
+# emission pattern limits
 n_values_z = fft.fftfreq(len(linspace(-k_max,k_max,gridpts_ds)), 2*k_max/gridpts_ds)
-emis_pat_limits = [min(n_values_z), max(n_values_z), min(n_values_z), max(n_values_z)]
-plt.imshow(abs(fft.ifftshift(fft.ifft2(iso)))**2, extent = emis_pat_limits)
+epl = [min(n_values_z), max(n_values_z), min(n_values_z), max(n_values_z)]
+zoom = 0.1
 
+# figure number (don't want to overwrite DS/Vg)
+epfn = sfn + 1
+for ef in ep_freqs:
+	plt.figure(epfn)
+	epfn += 1
+
+	plt.subplot(221)
+	plt.title('Slowness Surface, ' + str(round(10**-9*ef,2)) + ' GHz')
+	plt.xlabel('para [m]')
+	plt.ylabel('perp [m]')
+
+	lowb = ef - epsilon
+	highb = ef + epsilon
+	iso = where(vo < lowb, 0, where(vo > highb, 0, vo))
+	plt.imshow(iso, extent = disp_surface_limits)
+
+	plt.subplot(222)
+	plt.title('Intensity')
+	plt.xlabel('para [m]')
+	plt.ylabel('perp [m]')
+
+	plt.xlim(zoom*epl[0],zoom*epl[1])
+	plt.ylim(zoom*epl[2],zoom*epl[3])
+	plt.imshow(abs(fft.ifftshift(fft.ifft2(iso)))**2, extent = epl)
+
+	plt.subplot(223)
+	plt.title('Amplitude')
+	plt.xlabel('para [m]')
+	plt.ylabel('perp [m]')
+
+	plt.xlim(zoom*epl[0],zoom*epl[1])
+	plt.ylim(zoom*epl[2],zoom*epl[3])
+	plt.imshow(abs(fft.ifftshift(fft.ifft2(iso))), extent = epl)
+
+	plt.subplot(224)
+	plt.title('Phase')
+	plt.xlabel('para [m]')
+	plt.ylabel('perp [m]')
+
+	plt.xlim(zoom*epl[0],zoom*epl[1])
+	plt.ylim(zoom*epl[2],zoom*epl[3])
+	plt.imshow(angle(fft.ifftshift(fft.ifft2(iso))), extent = epl)
+	
 plt.tight_layout()
 plt.show()
